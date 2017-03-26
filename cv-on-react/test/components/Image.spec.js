7d3a239a256f0584db86c5c15cc34878120ce523
Aki Salmi <aki.salmi@iki.fi>
2017-03-26 13:55:42 +0300

    testing that displays correct flex item if props has items

--- cv-on-react/test/components/Image.spec.js
+++ cv-on-react/test/components/Image.spec.js
@@ -9,11 +9,19 @@ import HeadingImage from '../../src/components/Image.js'
 describe('Image', () => {
 
   it('should show fetching before data is present in props', () => {
-
     const props={ fetching: (<div className='fetching'>fetching</div>)}
+
     const wrapper = shallow(<HeadingImage {...props} />)
 
     expect( wrapper.find('.fetching')).to.have.length(1) 
+  })
+  it('should show fetching before data is present in props', () => {
+    const props={ me: {'in-short': { image : { src: 'src', alt: 'alt' }, quote: 'quote' }}} 
 
+    const wrapper = shallow(<HeadingImage {...props} />)
+
+    expect( wrapper.find('.fetching')).to.have.length(0) 
+    expect( wrapper.find('.flex')).to.have.length(1)
+    expect( wrapper.find('blockquote').text()).to.contain('quote')
   })
 })
